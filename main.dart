bb056fc2410112bc01008dba5b5df38900df0e90
Mahesh Reddy Avula <103478001+Mahesh0306@users.noreply.github.com>
2025-07-16 12:19:55 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,50 +1,101 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MyApp()
-  );
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal[700],
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Row(
-            children: <Widget> [
-              Container(
-                  width: 100,
-                  color: Colors.red,
-                  ),
-            Expanded(child: Column(
+              child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget> [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                ),
-              ],
-            ),),
-              Container(
-                  width: 100,
-                  height: 900,
-                  color: Colors.blue,
+                children: <Widget> [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage('images/mahesh.jpg'),
+                  ),
+                  Text(
+                    'Mahesh Reddy',
+                    style: TextStyle(
+                      fontFamily: 'Arvo',
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  Text(
+                    'SOFTWARE DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'Arvo',
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 180,
+                    height: 10,
+                    child: Divider(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 40),
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Row(
+                        children: <Widget> [
+                          Icon(Icons.phone),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          '+91 1234567890',
+                          style: TextStyle(
+                            fontFamily: 'Arvo',
+                            color: Colors.blueGrey[600],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 40),
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Row(
+                        children: <Widget> [
+                          Icon(Icons.email),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            'user@gmail.com',
+                            style: TextStyle(
+                              fontFamily: 'Arvo',
+                              color: Colors.blueGrey[600],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-          ],
-        ),
-      ),
-    ),
-    );
+            ),),
+      );
   }
 }
-
